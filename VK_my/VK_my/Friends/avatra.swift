74db474e32f8467d5242669cef2cc1486c513c87
KonoDioDa <kirill_ternovskii@mail.ru>
2022-05-23 15:53:37 +0500

    try

---
+++ VK_my/VK_my/Friends/avatra.swift
@@ -0,0 +1,20 @@
+//
+//  avatra.swift
+//  VK_my
+//
+//  Created by Кирилл Терновский on 23.05.2022.
+//
+
+import UIKit
+
+class avatra: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
