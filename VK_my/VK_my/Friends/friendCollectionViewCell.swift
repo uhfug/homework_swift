c62921c22c1ca2a724c896eeee1eb3b8156d08fb
KonoDioDa <kirill_ternovskii@mail.ru>
2022-05-26 20:07:50 +0500

    Homework_4

--- VK_my/VK_my/Friends/friendCollectionViewCell.swift
+++ VK_my/VK_my/Friends/friendCollectionViewCell.swift
@@ -10,6 +10,25 @@ import UIKit
 class friendCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet var imageAvatar: UIImageView!
-    
     @IBOutlet var nameFriends: UILabel!
+    
+    @IBOutlet var likeControl: likeConoller!
+    @IBOutlet var conteinerView: UIView!
+    
+    override func awakeFromNib() {
+        likeControl.likePicture.image = UIImage(systemName: "suit.heart")
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handlerTap(_:)))
+        tap.numberOfTapsRequired = 2
+        conteinerView.addGestureRecognizer(tap)
+    }
+    @objc func handlerTap(_:UITapGestureRecognizer) {
+        likeControl.isLiked.toggle()
+    
+        if likeControl.isLiked {
+            likeControl.likePicture.image = UIImage(systemName: "suit.heart.fill")
+        } else {
+            likeControl.likePicture.image = UIImage(systemName: "suit.heart")
+        }
+        
+    }
 }
